476f987c72d98376fd8cddbf8130b97400fb3def
hasnainchaudhry <h.chaudhry07@gmail.com>
2020-11-10 01:49:40 -0500

    This is H.W. #7 - - Part #0.).

--- features/steps/amazon_search.py
+++ features/steps/amazon_search.py
@@ -1,27 +1,40 @@
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from behave import given, when, then
+from app.application import Application
 
-SEARCH_INPUT =(By.ID, 'twotabsearchtextbox')
-SEARCH_ICON =(By.XPATH, "//input[@value = 'Go']")
-SEARCH_RESULT =(By.XPATH, "//span[@class='a-color-state a-text-bold']")
+SEARCH_INPUT = (By.ID, 'twotabsearchtextbox')
+SEARCH_ICON = (By.XPATH, "//input[@value = 'Go']")
+SEARCH_RESULT = (By.XPATH, "//span[@class='a-color-state a-text-bold']")
+SIGN_IN_BTN = (By.CSS_SELECTOR, "#nav-signin-tooltip >a")
 
-
+# H.W. #7 - Part #0.) - - Repeat everything I coded during the class.
 @given('Open Amazon page')
 def open_amazon(context):
-    context.driver.get('https://www.amazon.com/')
+    context.app.main_page.open_amazon()
 
 @when('Input Guitar into Amazon search field')
 def input_search_word(context):
-    context.driver.find_element(*SEARCH_INPUT).send_keys('Guitar')
+    context.app.main_page.input_search_word('Guitar')
 
 @when('Click on Amazon search icon')
 def click_search_icon(context):
-    search_icon = context.driver.find_element(*SEARCH_ICON)
-    search_icon.click()
+    context.app.main_page.click_search_icon()
 
 @then('Search results for Guitar are shown')
 def verify_search_result(context):
-    search_result = context.driver.find_element(*SEARCH_RESULT)
-    assert search_result.text == '"Guitar"', f'Error. Expected Guitar, but got {search_result.text}'
+    context.app.search_results_page.verify_search_result('"Guitar"')
+# This is the end of H.W. #7 - Part #0.). Thank you!
 
+# HW5
+# 0. Repeat everything I coded during the class.
+
+@then("Verify Sign In is clickable")
+def verify_sign_in_click(context):
+    context.driver.wait.until(EC.element_to_be_clickable(SIGN_IN_BTN))
+
+
+@then("Verify Sign In disappears")
+def verify_sign_in_disappears(context):
+    context.driver.wait.until(EC.invisibility_of_element_located(SIGN_IN_BTN))
 
